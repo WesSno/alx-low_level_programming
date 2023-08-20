730dfcfe7f5572cb7d905b9ce547bb3b17902aff
WesSno <kofibaafik@gmail.com>
2023-08-20 16:14:07 -0700

    all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
  * print_to_98 - A function that prints all natural numbers from
  * n to 98, followed by a new line.
  *
- * @n - variable to be used
+ *@n: variable to be used
  *
  * Return: Always 0
  */
